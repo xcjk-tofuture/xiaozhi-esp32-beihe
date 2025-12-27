6870a5bd836bccf8561437cb83c1e250f2a920a6
xcjk <2049368121@qq.com>
2025-12-27 18:23:19 +0800

    add:为按钮绑定功能，

--- main/boards/beihe/config.h
+++ main/boards/beihe/config.h
@@ -34,8 +34,8 @@
 // ILI9486 ��ʾ����
 #define DISPLAY_WIDTH   480
 #define DISPLAY_HEIGHT  320
-#define DISPLAY_MIRROR_X true
-#define DISPLAY_MIRROR_Y false
+#define DISPLAY_MIRROR_X false
+#define DISPLAY_MIRROR_Y true
 #define DISPLAY_SWAP_XY  true
 
 #define DISPLAY_OFFSET_X 0
